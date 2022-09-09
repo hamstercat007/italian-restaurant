5036d9e3c5f17ab92cf84b00cd1b89a3b63c1d47
hamstercat007 <lisamac421@gmail.com>
2022-09-09 15:19:20 +0100

    working

--- src/components/FoodModal.js
+++ src/components/FoodModal.js
@@ -7,29 +7,29 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure
+  useDisclosure,
 } from "@chakra-ui/react";
-import useCart from "./Cart";
+// import useCart from "./Cart";
 
-import {createRef, useState} from 'react'
+import { createRef, useState } from "react";
 
 export default function FoodModal({ title, price }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [number, setNumber] = useState(1)
+  const [number, setNumber] = useState(1);
 
   function incrementQuantity() {
-    setNumber(number + 1)
+    setNumber(number + 1);
   }
 
   function decrementQuantity() {
-    setNumber(number - 1)
+    setNumber(number - 1);
   }
 
-  function addItem() {
-    let total = price*number
-    let item = {name: title, quantity: number, price: total}
-    setCart([...cart, item])
-  }
+  // function addItem() {
+  //   let total = price*number
+  //   let item = {name: title, quantity: number, price: total}
+  //   setCart([...cart, item])
+  // }
 
   return (
     <>
@@ -41,20 +41,23 @@ export default function FoodModal({ title, price }) {
           <ModalHeader>{title}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-          <h1>Buy {title} for {price}</h1>
+            <h1>
+              Buy {title} for {price}
+            </h1>
           </ModalBody>
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button onClick={incrementQuantity}>
-              +
-            </Button>
+            <Button onClick={incrementQuantity}>+</Button>
             {number}
-            <Button onClick={decrementQuantity}>
-              -
+            <Button onClick={decrementQuantity}>-</Button>
+            <Button
+              colorScheme="orange"
+              style={{ marginLeft: "10px" }}
+            >
+              Add to Cart
             </Button>
-            <Button onClick={addItem} colorScheme="orange" style={{marginLeft:"10px"}}>Add to Cart</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
