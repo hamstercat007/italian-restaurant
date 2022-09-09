1a4f8cd76d18b30ebad3e9104883d49689e0be05
Phantom <sambaeverything@gmail.com>
2022-09-09 13:57:55 +0200

    the data object added to project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import foodData from './data/data.js';
 
+export default function App() {
+/*
 
-function App() {
+https://api.spoonacular.com/recipes/637672/information?apiKey=6e9b03c30e5e4bdeaa8e5428fb3062dd
+
+starters 
+637672 - Cheesy Picante Beef Macaroni
+658753 - Roma Tomato Bruschetta
+648320 - Jade Buddha Salmon Tartare
+
+main 
+795514 - Spaghetti Squash & Tomato Basil Meat Sauce
+632935 - Asparagus Lemon Risotto
+664737 - Veggie Lasagna Rolls with Peppery Pecorino Marinara
+desert 
+
+663351 - The Ultimate Tiramisu
+633415 - Baked Alaska
+1697581 - Crème Brûlée
+*/ 
+  //API Key: 6e9b03c30e5e4bdeaa8e5428fb3062dd
   const [foodImage, setFoodImage] = useState('https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg')
-  const [foodData, setFoodData] = useState({})
+  // const [foodData, setFoodData] = useState({})
   const [recipeID, setRecipeID] = useState('716429')
   
+  console.log('food data is......', foodData[0])
 
-  async function fetchFood(){
+  // async function fetchFood(){
 
 
-    const response = await fetch(`https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=e1833e085ad04e5583a2f8409022c612`);
-  const data = await response.json();
-  console.log('food info', data)
-  setFoodData(data)
-  console.log(data.)
-  }
+  //   const response = await fetch(`https://api.spoonacular.com/recipes/637672/information?apiKey=e1833e085ad04e5583a2f8409022c612`);
+  // const data = await response.json();
+  // console.log('food info', data)
+  // setFoodData(data)
+  // console.log(data)
+  // }
 
-  useEffect(() => {
-    fetchFood()
+  // useEffect(() => {
+  //   fetchFood()
     
-  }, [])
+  // }, [])
   
 
   return (
---
+++ src/data/data.js
@@ -0,0 +1,263 @@
+const foodData = [
+    {
+        "vegetarian": false,
+        "vegan": false,
+        "glutenFree": false,
+        "dairyFree": true,
+        "veryHealthy": false,
+        "cheap": false,
+        "veryPopular": false,
+        "sustainable": false,
+        "lowFodmap": false,
+        "weightWatcherSmartPoints": 17,
+        "gaps": "no",
+        "preparationMinutes": -1,
+        "cookingMinutes": -1,
+        "aggregateLikes": 1,
+        "healthScore": 12,
+        "pricePerServing": 236.52,
+        "extendedIngredients": [
+            {
+                "id": 6170,
+                "aisle": "Canned and Jarred",
+                "image": "beef-broth.png",
+                "consistency": "LIQUID",
+                "name": "beef stock",
+                "nameClean": "beef stock",
+                "original": "3 1/2 cups Swanson® Beef Broth or Swanson® Beef Stock",
+                "originalName": "Swanson® Beef Broth or Swanson® Beef Stock",
+                "amount": 3.5,
+                "unit": "cups",
+                "meta": [
+                    "swanson®"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 3.5,
+                        "unitShort": "cups",
+                        "unitLong": "cups"
+                    },
+                    "metric": {
+                        "amount": 828.058,
+                        "unitShort": "ml",
+                        "unitLong": "milliliters"
+                    }
+                }
+            },
+            {
+                "id": 6038,
+                "aisle": "Canned and Jarred",
+                "image": "cheddar-cheese-soup.png",
+                "consistency": "LIQUID",
+                "name": "cheddar cheese soup",
+                "nameClean": "cheddar cheese soup",
+                "original": "1 can (10 3/4 ounces) Campbell's® Condensed Cheddar Cheese Soup",
+                "originalName": "can Campbell's® Condensed Cheddar Cheese Soup",
+                "amount": 10.75,
+                "unit": "ounces",
+                "meta": [
+                    "canned"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 10.75,
+                        "unitShort": "oz",
+                        "unitLong": "ounces"
+                    },
+                    "metric": {
+                        "amount": 304.757,
+                        "unitShort": "g",
+                        "unitLong": "grams"
+                    }
+                }
+            },
+            {
+                "id": 10023572,
+                "aisle": "Meat",
+                "image": "fresh-ground-beef.jpg",
+                "consistency": "SOLID",
+                "name": "ground beef",
+                "nameClean": "ground chuck",
+                "original": "1 pound ground beef",
+                "originalName": "ground beef",
+                "amount": 1.0,
+                "unit": "pound",
+                "meta": [],
+                "measures": {
+                    "us": {
+                        "amount": 1.0,
+                        "unitShort": "lb",
+                        "unitLong": "pound"
+                    },
+                    "metric": {
+                        "amount": 453.592,
+                        "unitShort": "g",
+                        "unitLong": "grams"
+                    }
+                }
+            },
+            {
+                "id": 20420,
+                "aisle": "Pasta and Rice",
+                "image": "fusilli.jpg",
+                "consistency": "SOLID",
+                "name": "pasta",
+                "nameClean": "pasta",
+                "original": "3 cups uncooked shell-shaped pasta or 2 cups uncooked elbow macaroni",
+                "originalName": "uncooked shell-shaped pasta or 2 cups uncooked elbow macaroni",
+                "amount": 3.0,
+                "unit": "cups",
+                "meta": [
+                    "shell-shaped",
+                    "uncooked"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 3.0,
+                        "unitShort": "cups",
+                        "unitLong": "cups"
+                    },
+                    "metric": {
+                        "amount": 709.764,
+                        "unitShort": "ml",
+                        "unitLong": "milliliters"
+                    }
+                }
+            },
+            {
+                "id": 1006164,
+                "aisle": "Pasta and Rice;Ethnic Foods",
+                "image": "salsa.jpg",
+                "consistency": "SOLID",
+                "name": "picante sauce",
+                "nameClean": "picante sauce",
+                "original": "1/2 cup Pace® Picante Sauce",
+                "originalName": "Pace® Picante Sauce",
+                "amount": 0.5,
+                "unit": "cup",
+                "meta": [
+                    "pace®"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 0.5,
+                        "unitShort": "cups",
+                        "unitLong": "cups"
+                    },
+                    "metric": {
+                        "amount": 118.294,
+                        "unitShort": "ml",
+                        "unitLong": "milliliters"
+                    }
+                }
+            }
+        ],
+        "id": 637672,
+        "title": "Cheesy Picante Beef Macaroni",
+        "readyInMinutes": 45,
+        "servings": 4,
+        "sourceUrl": "http://www.foodista.com/recipe/6HDRLKKL/cheesy-picante-beef-macaroni",
+        "image": "https://spoonacular.com/recipeImages/637672-556x370.jpg",
+        "imageType": "jpg",
+        "summary": "Cheesy Picante Beef Macaroni is a <b>dairy free</b> main course. One serving contains <b>553 calories</b>, <b>31g of protein</b>, and <b>27g of fat</b>. For <b>$2.37 per serving</b>, this recipe <b>covers 20%</b> of your daily requirements of vitamins and minerals. 1 person has tried and liked this recipe. From preparation to the plate, this recipe takes approximately <b>45 minutes</b>. If you have pasta, campbell's® condensed cheddar cheese soup, ground beef, and a few other ingredients on hand, you can make it. To use up the pasta you could follow this main course with the <a href=\"https://spoonacular.com/recipes/chocolate-dessert-pasta-138646\">Chocolate Dessert Pasta</a> as a dessert. All things considered, we decided this recipe <b>deserves a spoonacular score of 54%</b>. This score is good. Try <a href=\"https://spoonacular.com/recipes/one-skillet-cheesy-beef-and-macaroni-631440\">One Skillet Cheesy Beef and Macaroni</a>, <a href=\"https://spoonacular.com/recipes/cheesy-macaroni-beef-skillet-617513\">Cheesy Macaroni-Beef Skillet</a>, and <a href=\"https://spoonacular.com/recipes/picante-beef-roast-422297\">Picante Beef Roast</a> for similar recipes.",
+        "cuisines": [],
+        "dishTypes": [
+            "lunch",
+            "main course",
+            "main dish",
+            "dinner"
+        ],
+        "diets": [
+            "dairy free"
+        ],
+        "occasions": [],
+        "winePairing": {
+            "pairedWines": [],
+            "pairingText": "No one wine will suit every pasta dish. Pasta in a tomato-based sauce will usually work well with a medium-bodied red, such as a montepulciano or chianti. Pasta with seafood or pesto will fare better with a light-bodied white, such as a pinot grigio. Cheese-heavy pasta can pair well with red or white - you might try a sangiovese wine for hard cheeses and a chardonnay for soft cheeses. We may be able to make a better recommendation if you ask again with a specific pasta dish.",
+            "productMatches": []
+        },
+        "instructions": "<ol><li>Cook the beef in a 10-inch skillet over medium-high heat until it's well browned, stirring often to separate meat. Pour off any fat.</li><li>Stir the broth in the skillet and heat to a boil. Stir in the pasta. Reduce the heat to medium and cook for 10 minutes or until the pasta is tender, stirring often.</li><li>Stir the soup and picante sauce in the skillet and cook until the mixture is hot and bubbling, stirring often.</li></ol>",
+        "analyzedInstructions": [
+            {
+                "name": "",
+                "steps": [
+                    {
+                        "number": 1,
+                        "step": "Cook the beef in a 10-inch skillet over medium-high heat until it's well browned, stirring often to separate meat.",
+                        "ingredients": [
+                            {
+                                "id": 23572,
+                                "name": "beef",
+                                "localizedName": "beef",
+                                "image": "beef-cubes-raw.png"
+                            },
+                            {
+                                "id": 1065062,
+                                "name": "meat",
+                                "localizedName": "meat",
+                                "image": "whole-chicken.jpg"
+                            }
+                        ],
+                        "equipment": [
+                            {
+                                "id": 404645,
+                                "name": "frying pan",
+                                "localizedName": "frying pan",
+                                "image": "pan.png"
+                            }
+                        ]
+                    },
+                    {
+                        "number": 2,
+                        "step": "Pour off any fat.Stir the broth in the skillet and heat to a boil. Stir in the pasta. Reduce the heat to medium and cook for 10 minutes or until the pasta is tender, stirring often.Stir the soup and picante sauce in the skillet and cook until the mixture is hot and bubbling, stirring often.",
+                        "ingredients": [
+                            {
+                                "id": 1006164,
+                                "name": "picante sauce",
+                                "localizedName": "picante sauce",
+                                "image": "salsa.jpg"
+                            },
+                            {
+                                "id": 1006615,
+                                "name": "broth",
+                                "localizedName": "broth",
+                                "image": "chicken-broth.png"
+                            },
+                            {
+                                "id": 20420,
+                                "name": "pasta",
+                                "localizedName": "pasta",
+                                "image": "fusilli.jpg"
+                            },
+                            {
+                                "id": 0,
+                                "name": "soup",
+                                "localizedName": "soup",
+                                "image": ""
+                            }
+                        ],
+                        "equipment": [
+                            {
+                                "id": 404645,
+                                "name": "frying pan",
+                                "localizedName": "frying pan",
+                                "image": "pan.png"
+                            }
+                        ],
+                        "length": {
+                            "number": 10,
+                            "unit": "minutes"
+                        }
+                    }
+                ]
+            }
+        ],
+        "sourceName": null,
+        "creditsText": null,
+        "originalId": null,
+        "spoonacularSourceUrl": "https://spoonacular.com/cheesy-picante-beef-macaroni-637672"
+    }
+]
+
+
+export default foodData
