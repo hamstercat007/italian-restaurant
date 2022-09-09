30b6516a801ef63365a57349cb69d52be3e9a83b
Phantom <sambaeverything@gmail.com>
2022-09-09 15:18:15 +0200

    d

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Flex, Spacer, Image } from '@chakra-ui/react';
+import { Flex, Spacer, Image, Button } from '@chakra-ui/react';
 
 
 export default function FoodItem({ title, image, price }) {
   return (
     <div>
-      <Flex >
+      <Flex  bg='gray.300' margin='10'>
+        <Flex>
       <Image src={image} alt={title} boxSize='150px'/>
       <h3>{title}</h3>
       <Spacer />
       <p>{price}</p>
+      </Flex>
+    
+      <Flex>
+        <Button colorScheme='blue'>Order</Button>
+        </Flex>
+      
+      
       </Flex>
     </div>
   );
