09f95ae3c0e3c975ecd865ae4d5dbb4545538c4c
Phantom <sambaeverything@gmail.com>
2022-09-09 13:04:23 +0200

    fetch testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
 
 
 function App() {
+  const [foodImage, setFoodImage] = useState('https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg')
+  const [foodData, setFoodData] = useState({})
+  const [recipeID, setRecipeID] = useState('716429')
+  
+
   async function fetchFood(){
-    
-    const response = await fetch('https://api.spoonacular.com/recipes/716429/information?apiKey=2d4f563ebff94a8f95bb7a8bf46d3e72');
+
+
+    const response = await fetch(`https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=e1833e085ad04e5583a2f8409022c612`);
   const data = await response.json();
-  console.log(data)
+  console.log('food info', data)
+  setFoodData(data)
+  console.log(data.)
   }
-  fetchFood()
+
+  useEffect(() => {
+    fetchFood()
+    
+  }, [])
+  
 
   return (
     <div className="App">
-      
+      <img src={foodImage}/>
     </div>
   );
 }
-
-export default App;
